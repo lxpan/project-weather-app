0f8d9ab644f540fbd22aa55ce12e92f4ce2897f4
Luoxi Pan <l.panpax@gmail.com>
2022-10-13 15:32:00 +1100

    Format using prettier

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const API_KEY = 'a9fa31006d3ec59a7888dead8b265f57';
 
 function KelvinToCelsius(kelvin) {
-    if (typeof (kelvin) === 'number') {
+    if (typeof kelvin === 'number') {
         const celsius = kelvin - 273.15;
         return celsius.toPrecision(4);
     }
@@ -9,7 +9,7 @@ function KelvinToCelsius(kelvin) {
 }
 
 function KelvinToFahrenheit(kelvin) {
-    if (typeof (kelvin) === 'number') {
+    if (typeof kelvin === 'number') {
         const fahrenheit = 1.8 * (kelvin - 273) + 32;
         return fahrenheit.toPrecision(4);
     }
@@ -17,22 +17,20 @@ function KelvinToFahrenheit(kelvin) {
 }
 
 async function getWeatherForCity(city) {
-    const processResponse = (result) => (
-        {
-            city,
-            weather: result.weather[0].main,
-            weather_description: result.weather[0].description,
-            temp: result.main.temp,
-            temp_celsius: KelvinToCelsius(result.main.temp),
-            temp_fahrenheit: KelvinToFahrenheit(result.main.temp),
-            feels_like_temp: result.main.feels_like,
-            temp_min: result.main.temp_min,
-            temp_max: result.main.temp_max,
-            coords: result.coord,
-            humidity: result.main.humidity,
-            wind: result.wind,
-        }
-    );
+    const processResponse = (result) => ({
+        city,
+        weather: result.weather[0].main,
+        weather_description: result.weather[0].description,
+        temp: result.main.temp,
+        temp_celsius: KelvinToCelsius(result.main.temp),
+        temp_fahrenheit: KelvinToFahrenheit(result.main.temp),
+        feels_like_temp: result.main.feels_like,
+        temp_min: result.main.temp_min,
+        temp_max: result.main.temp_max,
+        coords: result.coord,
+        humidity: result.main.humidity,
+        wind: result.wind,
+    });
 
     try {
         const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`);
