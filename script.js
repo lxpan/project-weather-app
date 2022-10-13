990fa33f7833964fa4b0838a84ad32b1b85eb4ee
Luoxi Pan <l.panpax@gmail.com>
2022-10-13 14:51:28 +1100

    Add functions to convert temperature units

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 const API_KEY = 'a9fa31006d3ec59a7888dead8b265f57';
 
+function KelvinToCelsius(kelvin) {
+    if (typeof (kelvin) === 'number') {
+        const celsius = kelvin - 273.15;
+        return celsius.toPrecision(4);
+    }
+    throw new Error('Temperature is not a number!');
+}
+
+function KelvinToFahrenheit(kelvin) {
+    if (typeof (kelvin) === 'number') {
+        const fahrenheit = 1.8 * (kelvin - 273) + 32;
+        return fahrenheit.toPrecision(4);
+    }
+    throw new Error('Temperature is not a number!');
+}
+
 function processResponse(result) {
     return {
         weather: result.weather[0].main,
         weather_description: result.weather[0].description,
         temp: result.main.temp,
+        temp_celsius: KelvinToCelsius(result.main.temp),
+        temp_fahrenheit: KelvinToFahrenheit(result.main.temp),
         feels_like_temp: result.main.feels_like,
         temp_min: result.main.temp_min,
         temp_max: result.main.temp_max,
