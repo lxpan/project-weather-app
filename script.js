91452603295e30d0c299813e287056a1b70f7eb5
Luoxi Pan <l.panpax@gmail.com>
2022-10-14 15:53:44 +1100

    Switch from mock data back to API data

--- script.js
+++ script.js
@@ -99,11 +99,12 @@ async function getWeatherForCity(city) {
     });
 
     try {
-        // const response = await fetch(
-        //     `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`
-        // );
-        // const data = await response.json();
-        const data = MOCK_DATA;
+        const response = await fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`
+        );
+        const data = await response.json();
+        console.log(data);
+        // const data = MOCK_DATA;
 
         return processResponse(data);
     } catch (error) {
