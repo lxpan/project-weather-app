d63df45cb14449e2488e1f5d2e6b9b372decb98e
Luoxi Pan <l.panpax@gmail.com>
2022-10-16 19:12:22 +1100

    Add loads and project data

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/script.js',
     output: {
         filename: 'script.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
 };
